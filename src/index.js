a5cf3d00373f7d1c2cb1418d320624df2149723a
OlegPetin <olegpetin87@gmail.com>
2023-01-22 20:07:30 +0300

    feat: make decode

--- src/index.js
+++ src/index.js
@@ -39,7 +39,59 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-}
+    let arr =[];
+    let morse = [];
+    let result = [];
+    let count = expr.length/10;
+    for(let i=0;i<count; i++){
+        arr[i]=expr.slice((i*10), (i*10) + 10);
+    }
+    arr.forEach(item => {
+        let letter ='';
+        for(i=0;i<item.length;i=i+2){
+            if(item[i] == '1' && item[i+1] == '1'){
+                letter = letter + '-';
+               // morse.push('-')
+            } else if(item[i] == '1' && item[i+1] == '0') {
+                letter = letter + '.';
+               // morse.push('.')
+            } else if(item[i] == '*'){
+                letter = letter + ' ';
+               // morse.push(' ');
+                i=i+8;
+            } 
+        }
+
+        morse.push(letter);
+    })
+    morse.forEach(item => {
+        if(item == ' '){
+            result.push(' ');
+        }
+        for ( let key in MORSE_TABLE){
+            if(item == key){
+                result.push(MORSE_TABLE[key]);
+            }
+        }
+    })
+    
+    // for(let j = arr[i].length-1;j=0;j=j-2){
+    //     if (arr[i][j]=== 0 && arr[i][j-1] === 1 ) {
+    //        morse.push('.');
+    //     } else {
+    //         if (arr[i][j]=== 1 && arr[i][j-1] === 1 ){
+    //             morse.push('-')
+    //         }
+    //         else {
+    //             morse.push(' ');
+    //         }
+    //     }
+    
+    //}
+    return result.join('');
+
+    }
+
 
 module.exports = {
     decode
